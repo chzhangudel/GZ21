86e0603f77715f4099fe90caba30c054cc82c47e
Arthur <arthur.guillaumin@gmail.com>
2021-06-03 22:08:49 -0400

    debugging

--- regression/functions.py
+++ regression/functions.py
@@ -54,6 +54,7 @@ def bz(velocity: np.ndarray):
     """
     Return the BZ parameterization
     """
+    velocity = velocity / 10
     zeta = (velocity['vsurf'].diff(dim='xu_ocean') / grid['dxu']
            - velocity['usurf'].diff(dim='yu_ocean') / grid['dyu'])
     d = (velocity['usurf'].diff(dim='yu_ocean') / grid['dyu']
@@ -66,5 +67,7 @@ def bz(velocity: np.ndarray):
     s_y = ((zeta * d_tilda).diff(dim='xu_ocean') / grid['dxu']
           + (zeta_sq + zeta * d).diff(dim='yu_ocean') / grid['dyu'])
     k_bt = -4.87 * 1e8
-    return s_x * 1e7 * k_bt, s_y * 1e7 * k_bt
+    s_x, s_y = s_x * 1e7 * k_bt, s_y * 1e7 * k_bt
+    print(s_x.std())
+    return s_x, s_y
 
