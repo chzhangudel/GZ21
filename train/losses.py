589c5113831ef655700b8409a72c8abc15ee371f
Arthur <arthur.guillaumin@gmail.com>
2021-06-14 15:22:42 -0400

    debugging

--- train/losses.py
+++ train/losses.py
@@ -488,7 +488,7 @@ class TuckeyGandHloss(_Loss):
         return epsilon
 
     def _transform_g_h(self, g, h):
-        return g / 3., h / 3.
+        return g / 10., h / 10.
 
 
 if __name__ == '__main__':
